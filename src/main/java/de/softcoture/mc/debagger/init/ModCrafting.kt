ff83dc2ef1d9b2e9a37a9151fbaf6403166336ab
Ovis Maximus <lorenz.debagger@softcoture.de>
2017-09-08 20:10:38 +0200

    Add smelting recipe for Obsidian Ingot by smelting Obsidian

--- src/main/java/de/softcoture/mc/debagger/init/ModCrafting.kt
+++ src/main/java/de/softcoture/mc/debagger/init/ModCrafting.kt
@@ -1,5 +1,6 @@
 package de.softcoture.mc.debagger.init
 
+import net.minecraft.init.Blocks
 import net.minecraft.item.ItemStack
 import net.minecraftforge.fml.common.registry.GameRegistry
 
@@ -10,5 +11,7 @@ object ModCrafting {
                 'R', ModItems.ruby)
         GameRegistry.addShapelessRecipe(ItemStack(ModItems.ruby, 9),
                 ModBlocks.blockRuby)
+        GameRegistry.addSmelting(Blocks.OBSIDIAN,
+                ItemStack(ModItems.obsidianIngot),0.5F)
     }
 }
